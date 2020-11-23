08ca24df7aabe68b26820104af1161b122d203ab
nemetha17 <47603257+nemetha17@users.noreply.github.com>
2020-11-23 10:05:55 +0100

    léptethető képek

---
+++ build/precache-manifest.f15193c4f22a79f9849c30b98bffcb23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd02ed40a5d6cb519adf0e9a68ba12d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7969e83f3b178eb6143e",
+    "url": "/static/js/2.b06abf86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b06abf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f12939945b36280afebf",
+    "url": "/static/js/main.d06cae8a.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, {useEffect, useState } from "react";
+import axios from 'axios'
+
 
 const App = () => {
-  return (
-    <div className="App">
-      Dogder
-      Dogder
-    </div>
-  );
-};
+  const [images, setImages] = useState([])
+  const [loaded, SetLoaded] = useState(false)
+
+const next = async () =>{
+  const resp = await axios("https://dog.ceo/api/breeds/image/random")
+  setImages(resp.data);
+}
+
+
+
+
+useEffect(()=>{
+  const load = async () => {
+      try {
+        const resp = await axios("https://dog.ceo/api/breeds/image/random")
+        setImages(resp.data);
+      } catch(err){
+        console.log(err)
+      }
+  }
+
+  if(!loaded) {
+    load()
+  }
+
+},[])
+
+
+return (
+  <div>
+    <h1>Dogder</h1>
+    <img src={images.message} alt={images.message} style={{ width: 400}} />
+    <button onClick={next}>Next</button>
+  </div>
+)
+
+}
 
 export default App;
