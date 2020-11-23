243c2509bbf9b3f4e6ace078880ec8562cc3e9ee
nemetha17 <47603257+nemetha17@users.noreply.github.com>
2020-11-23 11:32:51 +0100

    'Menthető kedvencek

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import axios from 'axios'
 
 const App = () => {
   const [images, setImages] = useState([])
-  const [loaded, SetLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false)
+  const[saved, setSaved]= useState([])
 
 const next = async () =>{
   const resp = await axios("https://dog.ceo/api/breeds/image/random")
   setImages(resp.data);
+}
+
+const save = image => {
+  if(!saved.includes(image)) {
+    setSaved([...saved,image])
+  }
+}
+
+const remove =(i) =>{
+  const remaining = saved.filter(image => image.message!== i.message)
+  setSaved(remaining)
 }
 
 
@@ -19,6 +31,7 @@ useEffect(()=>{
       try {
         const resp = await axios("https://dog.ceo/api/breeds/image/random")
         setImages(resp.data);
+        setLoaded(true)
       } catch(err){
         console.log(err)
       }
@@ -30,12 +43,28 @@ useEffect(()=>{
 
 },[])
 
+if (!loaded) {
+  return "Loading..."
+}
 
 return (
   <div>
+    {JSON.stringify(saved)}
     <h1>Dogder</h1>
     <img src={images.message} alt={images.message} style={{ width: 400}} />
+    <br />
+    <button onClick={() => save(images) }>{saved.includes(images)?'Unsave':'Save'}</button>
     <button onClick={next}>Next</button>
+
+
+    <ul>
+      {saved.map((image) =>(
+        <li key={image.message}>
+          {image.message}
+          <button onClick={() => remove(image)}>x</button>
+        </li>
+      ))}
+    </ul>
   </div>
 )
 
